7065a9746a4ae6886f1e32e8233f8e6ecd74eb91
YousefEbad <yousefomar6464@gmail.com>
2025-09-06 12:41:37 +0300

    Yousef Merge

--- src/screens/Home/home.tsx
+++ src/screens/Home/home.tsx
@@ -3,24 +3,10 @@ import BottomBar from "../../components/bottomBar/bottomBar";
 import { StyleSheet, Text } from 'react-native'
 
 const Home = () => {
-  return (
-    <>
-      <Text style={styles.container}>Home</Text>
-      <BottomBar />
-    </>
-  );
-};
+    return (
+        <View>
+        </View>
+    )
+}
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        color: "#ff0000ff",
-        fontSize: 34,
-        textAlign: "center",
-        marginTop: 50,
-      },
-
-})
-export default Home;
+export default Home
