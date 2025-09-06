569a4605ba55eaf849138fb75ae1383c8e339592
YousefEbad <yousefomar6464@gmail.com>
2025-09-06 12:42:04 +0300

    aa

--- src/screens/Home/home.tsx
+++ src/screens/Home/home.tsx
@@ -3,10 +3,24 @@ import BottomBar from "../../components/bottomBar/bottomBar";
 import { StyleSheet, Text } from 'react-native'
 
 const Home = () => {
-    return (
-        <View>
-        </View>
-    )
-}
+  return (
+    <>
+      <Text style={styles.container}>Home</Text>
+      <BottomBar />
+    </>
+  );
+};
 
-export default Home
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#ff0000ff",
+        fontSize: 34,
+        textAlign: "center",
+        marginTop: 50,
+      },
+
+})
+export default Home;
